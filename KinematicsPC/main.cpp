f9f6baae78a3f1868e0f8d4b724c06a12a5532fc
justincdavis <davis.justin@mssm.org>
2019-05-07 13:25:45 -0500

    almost done with a base kinematics algorithm with joint limits

--- KinematicsPC/main.cpp
+++ KinematicsPC/main.cpp
@@ -3,6 +3,9 @@
 #include "chain.hpp"
 #include "chainfksolver.hpp"
 #include "chainfksolverpos_recursive.hpp"
+#include "chainiksolverpos_lma.hpp"
+#include "chainiksolvervel_wdls.hpp"
+#include "chainiksolverpos_nr_jl.hpp"
 #include "frames_io.hpp"
 #include <stdio.h>
 #include <iostream>
@@ -11,48 +14,131 @@ using namespace KDL;
 
 int main(int argc, char *argv[])
 {
-
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
 
     //BUILDING THE LEGS
     Chain leftLeg = Chain();
     Chain rightLeg = Chain();
 
-    //Create the left leg chain
-    Joint torsoToHipJoint(Joint::None);
-    Frame torsoToHipFrame = Frame(Vector(4.454, 0, 0));
-    leftLeg.addSegment(Segment(torsoToHipJoint, torsoToHipFrame));
+    //CREATING THE LEFT LEG CHAIN
+    Joint ltorsoToHipJoint(Joint::None);
+    Frame ltorsoToHipFrame = Frame(Vector(.04454, 0, 0));
+    leftLeg.addSegment(Segment(ltorsoToHipJoint, ltorsoToHipFrame));
 
-    Joint hipToHipJoint(Joint::RotZ);
-    Frame hipToHipFrame = Frame(Rotation::EulerXYZ( M_PI, 0, 0)) * Frame(Vector(0, 0, 5.458));
-    leftLeg.addSegment(Segment(hipToHipJoin, hipToHipFrame));
+    Joint lhipToHipJoint(Joint::RotZ);
+    Frame lhipToHipFrame = Frame(Rotation::EulerZYX(0, 0, M_PI)) * Frame(Vector(0, 0, .05458));
+    leftLeg.addSegment(Segment(lhipToHipJoint, lhipToHipFrame));
 
-    Joint hipToThighJoint(Joint::RotZ);
-    Frame hipToThighFrame = Frame(Rotation::EulerXYZ(-M_PI / 2, 0, 0)) * Frame(Vector(0, 2.908, 0));
-    leftLeg.addSegment(Segment(hipToThighJoint, hipToThighFrame));
+    Joint lhipToThighJoint(Joint::RotZ);
+    Frame lhipToThighFrame = Frame(Rotation::EulerZYX(0, 0, -M_PI / 2)) * Frame(Vector(0, .02908, 0));
+    leftLeg.addSegment(Segment(lhipToThighJoint, lhipToThighFrame));
 
-    Joint thighToKneeJoint(Joint::RotZ);
-    Frame thighToKneeFrame = Frame(Rotation::EulerXYZ(0, M_PI / 2, 0)) * Frame(Vector(0, 7.46, 0));
-    leftLeg.addSegment(Segment(thighToKneeJoint, thighToKneeFrame));
+    Joint lthighToKneeJoint(Joint::RotZ);
+    Frame lthighToKneeFrame = Frame(Rotation::EulerZYX(0, M_PI / 2, 0)) * Frame(Vector(0, .0746, 0));
+    leftLeg.addSegment(Segment(lthighToKneeJoint, lthighToKneeFrame));
 
-    Joint kneeToAnkleJoint(Joint::RotZ);
-    Frame kneeToAnkleFrame = Frame(Vector(0, 5.729, 0));
-    leftLeg.addSegment(Segment(kneeToAnkleJoint, kneeToAnkleFrame));
+    Joint lkneeToAnkleJoint(Joint::RotZ);
+    Frame lkneeToAnkleFrame = Frame(Vector(0, .05729, 0));
+    leftLeg.addSegment(Segment(lkneeToAnkleJoint, lkneeToAnkleFrame));
 
-    Joint ankleToFootJoint(Joint::RotZ);
-    Frame ankleToFootFrame = Frame(Vector(0, 5.497, 0));
-    leftLeg.addSegment(Segment(ankleToFootJoint, ankleToFootFrame));
+    Joint lankleToFootJoint(Joint::RotZ);
+    Frame lankleToFootFrame = Frame(Vector(0, .05497, 0));
+    leftLeg.addSegment(Segment(lankleToFootJoint, lankleToFootFrame));
 
-    Joint footToFootJoint(Joint::RotZ);
-    Frame footToFootFrame = Frame(Rotation::EulerXYZ(0, M_PI / 2, 0)) * Frame(Vector(0, 2.306, 0));
-    leftLeg.addSegment(Segment(footToFootJoint, footToFootFrame));
+    Joint lfootToFootJoint(Joint::RotZ);
+    Frame lfootToFootFrame = Frame(Rotation::EulerZYX(0, M_PI / 2, 0)) * Frame(Vector(0, .02306, 0));
+    leftLeg.addSegment(Segment(lfootToFootJoint, lfootToFootFrame));
 
-    //Create the right leg chain
+    //CREATING THE RIGHT LEG CHAIN
+    Joint rtorsoToHipJoint(Joint::None);
+    Frame rtorsoToHipFrame = Frame(Vector(.04454, 0, 0));
+    rightLeg.addSegment(Segment(rtorsoToHipJoint, rtorsoToHipFrame));
+
+    Joint rhipToHipJoint(Joint::RotZ);
+    Frame rhipToHipFrame = Frame(Rotation::EulerZYX(0, 0, -M_PI)) * Frame(Vector(0, 0, .05458));
+    rightLeg.addSegment(Segment(rhipToHipJoint, rhipToHipFrame));
+
+    Joint rhipToThighJoint(Joint::RotZ);
+    Frame rhipToThighFrame = Frame(Rotation::EulerZYX(0, 0, M_PI / 2)) * Frame(Vector(0, .02908, 0));
+    rightLeg.addSegment(Segment(rhipToThighJoint, rhipToThighFrame));
+
+    Joint rthighToKneeJoint(Joint::RotZ);
+    Frame rthighToKneeFrame = Frame(Rotation::EulerZYX(0, -M_PI / 2, 0)) * Frame(Vector(0, .0746, 0));
+    rightLeg.addSegment(Segment(rthighToKneeJoint, rthighToKneeFrame));
+
+    Joint rkneeToAnkleJoint(Joint::RotZ);
+    Frame rkneeToAnkleFrame = Frame(Vector(0, .05729, 0));
+    rightLeg.addSegment(Segment(rkneeToAnkleJoint, rkneeToAnkleFrame));
+
+    Joint rankleToFootJoint(Joint::RotZ);
+    Frame rankleToFootFrame = Frame(Vector(0, .05497, 0));
+    rightLeg.addSegment(Segment(rankleToFootJoint, rankleToFootFrame));
+
+    Joint rfootToFootJoint(Joint::RotZ);
+    Frame rfootToFootFrame = Frame(Rotation::EulerZYX(0, -M_PI / 2, 0)) * Frame(Vector(0, .02306, 0));
+    rightLeg.addSegment(Segment(rfootToFootJoint, rfootToFootFrame));
+
+    //DEFINING MAX AND MINS FOR THE SERVO JOINTS
+    JntArray leftJointArrayMin = JntArray(7);
+    JntArray leftJointArrayMax = JntArray(7);
+    JntArray rightJointArrayMin = JntArray(7);
+    JntArray rightJointArrayMax = JntArray(7);
+
+    leftJointArrayMin(0) = 0;
+    leftJointArrayMin(1) = -0.7853981634;
+    leftJointArrayMin(2) = -0.7853981634;
+    leftJointArrayMin(3) = -0.7853981634;
+    leftJointArrayMin(4) = -0.7853981634;
+    leftJointArrayMin(5) = -0.7853981634;
+    leftJointArrayMin(6) = -0.7853981634;
+
+    leftJointArrayMax(0) = 0;
+    leftJointArrayMax(1) = 0.7853981634;
+    leftJointArrayMax(2) = 0.7853981634;
+    leftJointArrayMax(3) = 0.7853981634;
+    leftJointArrayMax(4) = 0.7853981634;
+    leftJointArrayMax(5) = 0.7853981634;
+    leftJointArrayMax(6) = 0.7853981634;
+
+    rightJointArrayMin(0) = 0;
+    rightJointArrayMin(1) = -0.7853981634;
+    rightJointArrayMin(2) = -0.7853981634;
+    rightJointArrayMin(3) = -0.7853981634;
+    rightJointArrayMin(4) = -0.7853981634;
+    rightJointArrayMin(5) = -0.7853981634;
+    rightJointArrayMin(6) = -0.7853981634;
+
+    rightJointArrayMax(0) = 0;
+    rightJointArrayMax(1) = 0.7853981634;
+    rightJointArrayMax(2) = 0.7853981634;
+    rightJointArrayMax(3) = 0.7853981634;
+    rightJointArrayMax(4) = 0.7853981634;
+    rightJointArrayMax(5) = 0.7853981634;
+    rightJointArrayMax(6) = 0.7853981634;
+
+    //FORWARD KINEMATIC SOLVERS
+    ChainFkSolverPos_recursive leftFkSolver = ChainFkSolverPos_recursive(leftLeg);
+    ChainFkSolverPos_recursive rightFkSolver = ChainFkSolverPos_recursive(rightLeg);
+
+    //INVERSE KINEMATIC SOLVER
+    ChainIkSolverVel_wdls leftIkSolverVel = ChainIkSolverVel_wdls(leftLeg);
+    ChainIkSolverVel_wdls rightIkSolverVel = ChainIkSolverVel_wdls(rightLeg);
+
+    ChainIkSolverPos_NR_JL leftIkSolverPos = ChainIkSolverPos_NR_JL(leftLeg, leftJointArrayMin, leftJointArrayMax, leftFkSolver, leftIkSolverVel);
+    ChainIkSolverPos_NR_JL rightIkSolverPos = ChainIkSolverPos_NR_JL(rightLeg, rightJointArrayMin, rightJointArrayMax, rightFkSolver, rightIkSolverVel);
+
+    //RESULTS JOINT ARRAY
+    JntArray leftResults[7];
+    JntArray rightResults[7];
+
+    //GOAL FRAMES FOR THE FEET
 
 
+    //RUNNING THE ALGORITHM
+    int leftRet =
+    int rightRet =
 
     return a.exec();
 }
